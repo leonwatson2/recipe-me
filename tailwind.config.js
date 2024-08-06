f14f20365930dd4cbdd89d6274b778b81d450e8c
Leon Watson <leon@vlw2.com>
2024-08-05 20:41:08 -0500

    Added custom colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    colors: {
+      primary: "#D1774c",
+      black: "#332B27",
+      grey:"#524A46",
+      brown: "#a77158",
+      lbrown: "#C29986"
+    },
     extend: {},
   },
   plugins: [],
