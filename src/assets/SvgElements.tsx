9b776a69e7e52eefe99d5cb119c6ef9ea3a0e46d
Leon Watson <leon@vlw2.com>
2024-08-16 15:40:37 -0500

    added profile head to svgs

--- src/assets/SvgElements.tsx
+++ src/assets/SvgElements.tsx
@@ -1,7 +1,7 @@
 import { FC } from "react";
 
 // prettier-ignore
-type SVGTitles = | "the-logo" | "bf-logo" | "buy-clock-icon" | "play-button" | "pinterest" | "facebook" | "emailSVG" | "instagram" | "link" | "sms" | "twitter" | "youtube" | "whatsapp" | "x" | "search-icon" | "clock-icon" | "right-arrow" | "arrow-down" | "caret-down" | "caret-up" | "caret-left" | "caret-right" | "hamburger" | "pop-out" | "thumbs-up" | "thumbs-up-buy" | "speech" | "audio-on" | "replay" | "plus" | "minus" | "walmart-grocery-logo" | "error-smiley" | "magnifying-glass-no-results" | "swap" | "more" | "grocerybag" | "checkmark" | "shop-merch" | "cart-not-selected" | "cart-selected" | "facebookMonochrome" | "pinterestMonochrome" | "emailMonochrome" | "printMonochrome" | "smsMonochrome" | "twitterMonochrome" | "whatsappMonochrome" | "comment" | "tipsHighlights";
+type SVGTitles = | "the-logo" | "bf-logo" | "buy-clock-icon" | "play-button" | "pinterest" | "facebook" | "emailSVG" | "instagram" | "link" | "sms" | "twitter" | "youtube" | "whatsapp" | "x" | "search-icon" | "clock-icon" | "right-arrow" | "arrow-down" | "caret-down" | "caret-up" | "caret-left" | "caret-right" | "hamburger" | "pop-out" | "thumbs-up" | "thumbs-up-buy" | "speech" | "audio-on" | "replay" | "plus" | "minus" | "walmart-grocery-logo" | "error-smiley" | "magnifying-glass-no-results" | "swap" | "more" | "grocerybag" | "checkmark" | "shop-merch" | "cart-not-selected" | "cart-selected" | "facebookMonochrome" | "pinterestMonochrome" | "emailMonochrome" | "printMonochrome" | "smsMonochrome" | "twitterMonochrome" | "whatsappMonochrome" | "comment" | "tipsHighlights" | "profile";
 
 export function SvgElements() {
   return (
@@ -10,6 +10,10 @@ export function SvgElements() {
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
     >
+      <symbol id="profile" viewBox="0 -960 960 960">
+        <title>Profile</title>
+        <path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Zm80-80h480v-32q0-11-5.5-20T700-306q-54-27-109-40.5T480-360q-56 0-111 13.5T260-306q-9 5-14.5 14t-5.5 20v32Zm240-320q33 0 56.5-23.5T560-640q0-33-23.5-56.5T480-720q-33 0-56.5 23.5T400-640q0 33 23.5 56.5T480-560Zm0-80Zm0 400Z" />
+      </symbol>
       <symbol id="the-logo" viewBox="0 0 150 76">
         <title>The Logo</title>
         <g fill="none" fillRule="evenodd">
