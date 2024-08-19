5b26f8d2640e01b1aab50933807a3584b6e99e0d
Leon Watson <leon@vlw2.com>
2024-08-19 17:41:13 -0500

    Added context to root path

--- src/components/Root.tsx
+++ src/components/Root.tsx
@@ -1,14 +1,21 @@
 import { Outlet } from "react-router-dom";
 import { MainNav } from "./Navigation";
 import { SvgElements } from "../assets/SvgElements";
-
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { GOOGLE_AUTH_CLIENT_ID } from "../firebase/config";
+import { UserContext, UserContextType, useUser } from "./auth";
 export function Root() {
+
+  const userContext: UserContextType = useUser()
   return (
-    <>
-      <MainNav />
-      <SvgElements />
-      <Outlet />
-    </>
+    <GoogleOAuthProvider clientId={GOOGLE_AUTH_CLIENT_ID}>
+      <UserContext.Provider value={userContext} >
+
+        <MainNav />
+        <SvgElements />
+        <Outlet />
+      </UserContext.Provider>
+    </GoogleOAuthProvider>
   );
 }
 
