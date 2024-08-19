b5062446e901621438cc7b48221c8261c67c4e75
leonwatson2 <code@vlw2.com>
2024-08-19 14:31:16 -0500

    Updating dialog component

--- src/components/utils/DialogBox.tsx
+++ src/components/utils/DialogBox.tsx
@@ -7,7 +7,10 @@ type DialogBoxProps = {
 export const DialogBox = forwardRef<HTMLDialogElement, DialogBoxProps>(
   ({ children, title, className }, dialogRef) => {
     return (
-      <dialog ref={dialogRef} className="w-full max-w-7xl bg-brown text-white">
+      <dialog
+        ref={dialogRef}
+        className="absolute mt-auto w-full top-96 max-w-7xl max-h-none bg-brown text-white"
+      >
         <div className={"w-full h-full " + className}>
           <h2 className="col-span-full text-3xl py-6 text-center">{title}</h2>
           {children}
