41d256d9bc8b2716777306ca7a1dee90da5bb537
Leon Watson <leon@vlw2.com>
2024-08-19 17:45:07 -0500

    updated google login to now have error fn

--- src/components/auth/GoogleLoginDialog.tsx
+++ src/components/auth/GoogleLoginDialog.tsx
@@ -5,39 +5,37 @@ import { getGoogleUserData } from "./actions";
 import { useUser } from "./context";
 
 type GoogleLoginDialog = {
-  setError: (error: string) => void
 };
 
-export const GoogleLoginDialog = forwardRef<HTMLDialogElement, GoogleLoginDialog>((
-  { setError },
+export const GoogleLoginDialog = forwardRef<HTMLDialogElement, GoogleLoginDialog>((_,
   ref,
 ) => {
   const { login, loggedIn, logout } = useUser()
 
-  
+
   return (
     <DialogBox
       title="Login"
       ref={ref}
       className="grid grid-cols-1 justify-center md:max-w-3/4 "
     >
-    <div className="grid justify-center m-0 pb-10">
-    { loggedIn ? 
-        <button className="button bg-lbrown h-12 w-32 drop-shadow-md" onClick={logout}> Logout </button>
-        : 
-        <GoogleLogin
-          onSuccess={(crendentialResponse) => {
-            getGoogleUserData(crendentialResponse).then((googleUser)=>{
-              login(googleUser)
-            }).catch(e=>console.log(e))
+      <div className="grid justify-center m-0 pb-10">
+        {loggedIn ?
+          <button className="button bg-lbrown h-12 w-32 drop-shadow-md" onClick={logout}> Logout </button>
+          :
+          <GoogleLogin
+            onSuccess={(crendentialResponse) => {
+              getGoogleUserData(crendentialResponse).then((googleUser) => {
+                login(googleUser)
+              }).catch(e => console.log(e))
 
             }
-          }
-        onError={() => {
-          setError("Error while logging in");
-        }}
-        />} 
-        </div>
-        </DialogBox>
+            }
+            onError={() => {
+              console.log("Error while logging in");
+            }}
+          />}
+      </div>
+    </DialogBox>
   );
 })
