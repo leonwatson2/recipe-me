dc94f08002d0cb712744c929ed6bfecac01103eb
Leon Watson <leon@vlw2.com>
2024-08-19 21:12:53 -0500

    Added cancel button to google dialog

--- src/components/auth/GoogleLoginDialog.tsx
+++ src/components/auth/GoogleLoginDialog.tsx
@@ -3,6 +3,7 @@ import { DialogBox } from "../utils/DialogBox";
 import { GoogleLogin } from "@react-oauth/google";
 import { getGoogleUserData } from "./actions";
 import { useUserContext } from "./context";
+import { Button } from "../utils";
 
 type GoogleLoginDialog = {
   onSuccess?: () => void;
@@ -21,33 +22,38 @@ export const GoogleLoginDialog = forwardRef<
     <DialogBox
       title={loggedIn ? "Logout" : "Login"}
       ref={innerRef}
-      className="grid grid-cols-1 justify-center md:max-w-3/4 "
+      className="grid grid-cols-2 gap-7 justify-center md:max-w-3/4 "
     >
-      <div className="grid justify-center m-0 pb-10">
-        {loggedIn ? (
-          <button
-            className="button bg-lbrown h-12 w-32 drop-shadow-md"
-            onClick={logout}
-          >
-            Logout
-          </button>
-        ) : (
+      <Button
+        onClick={
+          () => innerRef?.current?.close()
+        }>
+        Cancel
+      </Button>
+      {loggedIn ? (
+        <Button
+          onClick={logout}
+        >
+          Logout
+        </Button>
+      ) : (
+        <div className="grid justify-center m-0">
           <GoogleLogin
-            onSuccess={(crendentialResponse) => {
-              getGoogleUserData(crendentialResponse)
+            onSuccess={async (crendentialResponse) => {
+              await getGoogleUserData(crendentialResponse)
                 .then((googleUser) => {
                   login(googleUser);
+                  innerRef?.current?.close();
                   onSuccess && onSuccess();
                 })
                 .catch((e) => console.log(e));
-              innerRef?.current?.close();
             }}
             onError={() => {
               console.log("Error while logging in");
             }}
           />
-        )}
-      </div>
+        </div>
+      )}
     </DialogBox>
   );
 });
