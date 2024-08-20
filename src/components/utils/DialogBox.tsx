90b10215ae192b6be27f34dcb602f2600e66bda2
Leon Watson <leon@vlw2.com>
2024-08-19 21:12:28 -0500

    Updated dialog box

--- src/components/utils/DialogBox.tsx
+++ src/components/utils/DialogBox.tsx
@@ -10,8 +10,11 @@ export const DialogBox = forwardRef<HTMLDialogElement, DialogBoxProps>(
       <dialog
         ref={dialogRef}
         className="absolute mt-auto w-full top-96 max-w-7xl max-h-none bg-brown text-white drop-shadow-lg"
+        onClick={()=>{
+          
+        }}
       >
-        <div className={"w-full h-full " + className}>
+        <div className={"w-full h-full p-7 " + className}>
           <h2 className="col-span-full text-3xl py-6 text-center">{title}</h2>
           {children}
         </div>
