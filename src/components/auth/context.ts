6c92e92c950b2b9fef6ee3cfbbafa4949fefcd08
Leon Watson <leon@vlw2.com>
2024-08-21 01:26:40 -0500

    formatting

--- src/components/auth/context.ts
+++ src/components/auth/context.ts
@@ -1,4 +1,10 @@
-import { createContext, useCallback, useContext, useEffect, useState } from "react";
+import {
+  createContext,
+  useCallback,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
 import { GoogleUser, User } from "./types";
 import { loginUser } from "../../firebase/actions";
 import { useNavigate } from "react-router-dom";
@@ -9,16 +15,15 @@ export type UserContextType = {
   loggedIn: boolean;
   loadingUser: boolean;
   user?: User;
-  login: (googleUser: GoogleUser) => void,
-  logout: () => void,
-
+  login: (googleUser: GoogleUser) => void;
+  logout: () => void;
 };
 
 const defaultUserContext: UserContextType = {
   loggedIn: false,
   loadingUser: false,
-  login: () => { },
-  logout: () => { },
+  login: () => {},
+  logout: () => {},
 };
 
 export const UserContext = createContext<UserContextType>(defaultUserContext);
@@ -29,82 +34,84 @@ export const useUserContext = () => {
 };
 
 export const useProtectedRoute = (isAllowed: boolean, redirect?: string) => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   useEffect(() => {
     if (isAllowed) return;
-    navigate(redirect ? redirect : '/')
-  }, [isAllowed, redirect, navigate])
-}
+    navigate(redirect ? redirect : "/");
+  }, [isAllowed, redirect, navigate]);
+};
 
-const LS_CREDS = 'ga-creds'
-const LS_EXPIRE = 'rs-expire'
-const EXPIRE_DURATION_DAYS = 2
+const LS_CREDS = "ga-creds";
+const LS_EXPIRE = "rs-expire";
+const EXPIRE_DURATION_DAYS = 2;
 
-export const updateUserLocalStorage = (credential:string) => {
-  if(credential.length === 0) return
-  const expireDate = new Date()
-  expireDate.setDate(expireDate.getDate()+EXPIRE_DURATION_DAYS)
-  localStorage.setItem(LS_CREDS, credential)
-  localStorage.setItem(LS_EXPIRE, expireDate.toJSON())
-}
+export const updateUserLocalStorage = (credential: string) => {
+  if (credential.length === 0) return;
+  const expireDate = new Date();
+  expireDate.setDate(expireDate.getDate() + EXPIRE_DURATION_DAYS);
+  localStorage.setItem(LS_CREDS, credential);
+  localStorage.setItem(LS_EXPIRE, expireDate.toJSON());
+};
 const clearUserLocalStorage = () => {
-  localStorage.removeItem(LS_CREDS)
-  localStorage.removeItem(LS_EXPIRE)
-}
+  localStorage.removeItem(LS_CREDS);
+  localStorage.removeItem(LS_EXPIRE);
+};
 const hasValidUserStorage = () => {
-
- if (localStorage.getItem(LS_CREDS)) {
-    const expireDateJSON = localStorage.getItem(LS_EXPIRE) as string
-    const expireDate = new Date(expireDateJSON)
-    const currentTime = new Date()
-    if(expireDate.valueOf() > currentTime.valueOf()){ 
+  if (localStorage.getItem(LS_CREDS)) {
+    const expireDateJSON = localStorage.getItem(LS_EXPIRE) as string;
+    const expireDate = new Date(expireDateJSON);
+    const currentTime = new Date();
+    if (expireDate.valueOf() > currentTime.valueOf()) {
       return true;
     }
   }
-}
+};
 
 export const useUser: () => UserContextType = () => {
-  const [user, setUser] = useState<User>()
-  const [googleUser, setGoogleUser] = useState<GoogleUser>()
-  const [loggedIn, setLoggedIn] = useState(false)
-  const [loadingUser, setLoading] = useState(false)
+  const [user, setUser] = useState<User>();
+  const [googleUser, setGoogleUser] = useState<GoogleUser>();
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [loadingUser, setLoading] = useState(false);
 
   useEffect(() => {
     if (loggedIn && user) {
-      return
+      return;
     } else if (hasValidUserStorage()) {
-      const credential = localStorage.getItem(LS_CREDS) as string
-      updateUserLocalStorage(credential)
-      loginUser(getGoogleUserData(credential).email).then((userRes) => {
-        setUser(userRes);
-        setLoggedIn(true)
-
-      }).catch(e => console.log(e))
+      const credential = localStorage.getItem(LS_CREDS) as string;
+      updateUserLocalStorage(credential);
+      loginUser(getGoogleUserData(credential).email)
+        .then((userRes) => {
+          setUser(userRes);
+          setLoggedIn(true);
+        })
+        .catch((e) => console.log(e));
     } else {
-      clearUserLocalStorage()
+      clearUserLocalStorage();
     }
+  }, [loggedIn, user]);
 
-  }, [loggedIn, user])
-
-
-  const login = useCallback(async (googleUser: GoogleUser) => {
-    setLoading(true)
-    await loginUser(googleUser.email).then((userRes) => {
-      setUser(userRes);
-      setLoggedIn(true)
-      setGoogleUser(googleUser)
-    }).finally(() => {
-      setLoading(false)
-
-    })
-  }, [setGoogleUser, setLoggedIn])
+  const login = useCallback(
+    async (googleUser: GoogleUser) => {
+      setLoading(true);
+      await loginUser(googleUser.email)
+        .then((userRes) => {
+          setUser(userRes);
+          setLoggedIn(true);
+          setGoogleUser(googleUser);
+        })
+        .finally(() => {
+          setLoading(false);
+        });
+    },
+    [setGoogleUser, setLoggedIn],
+  );
 
   const logout = useCallback(() => {
-    setLoggedIn(false)
-    setGoogleUser(undefined)
-    setUser(undefined)
-    clearUserLocalStorage()
-  }, [])
+    setLoggedIn(false);
+    setGoogleUser(undefined);
+    setUser(undefined);
+    clearUserLocalStorage();
+  }, []);
 
   return {
     user,
@@ -112,7 +119,6 @@ export const useUser: () => UserContextType = () => {
     loggedIn,
     login,
     logout,
-    loadingUser
-  }
-
-}
+    loadingUser,
+  };
+};
