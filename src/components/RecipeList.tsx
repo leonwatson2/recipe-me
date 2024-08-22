a2010e93def035575afe2849b49615dae02d415e
leonwatson2 <leon@vlw2.com>
2024-08-22 15:22:22 -0500

    made recipe list more responsive

--- src/components/RecipeList.tsx
+++ src/components/RecipeList.tsx
@@ -2,6 +2,7 @@ import "react";
 import "../firebase/config.ts";
 import { Recipe } from "../types";
 import { Link, useLoaderData } from "react-router-dom";
+import { SVG } from "../assets/SvgElements.tsx";
 
 export const RecipeList = () => {
   const recipes = useLoaderData() as Array<Recipe>;
@@ -9,14 +10,16 @@ export const RecipeList = () => {
   return (
     <section className="mx-auto max-w-7xl relative pt-7">
       <header className="text-7xl font-bold  mb-7">Lastest Recipes</header>
-      <ul className="grid lg:grid-cols-5 gap-4 auto-fill-40">
+      <ul className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 auto-fill">
         {recipes.map((r) => (
           <li
             key={r.id}
             className="text-3xl w-full transition-transform hover:translate-x-2 hover:underline mb-10"
           >
-            <Link to={"recipe/" + r.slug}>
-              {" "}
+            <Link to={"recipe/" + r.slug} className="w-full h-full ">
+              <div className="bg-brown flex h-54 w-54">
+                <SVG className="fill-black" title="smsMonochrome" />
+              </div>
               <p>{r.name}</p>
               <p className="text-base">{r.cookTime} minutes</p>
             </Link>
