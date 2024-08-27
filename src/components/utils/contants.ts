839c74bde6c2673b8b8eb32184ce583b813dc262
leonwatson2 <leon@vlw2.com>
2024-08-27 00:12:08 -0500

    pagination limit increased

--- src/components/utils/contants.ts
+++ src/components/utils/contants.ts
@@ -3,4 +3,4 @@ export const LS_CREDS = 'ga-creds'
 export const LS_EXPIRE = 'rs-expire'
 export const EXPIRE_DURATION_DAYS = 2
 export const SEARCH_TERM_KEY = 's'
-export const QUERY_DOC_LIMIT = 5
+export const QUERY_DOC_LIMIT = 15
