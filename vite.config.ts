85d1e0bf592b37488dadd130fb1086cc76e29766
Leon Watson <leon@vlw2.com>
2024-09-07 04:03:18 -0500

    added chunking and path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,31 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwind from 'tailwindcss'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import tailwind from "tailwindcss";
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
-      plugins:[tailwind()]
-    }
+      plugins: [tailwind()],
+    },
   },
   resolve: {
     alias: {
-      '@Utils': path.resolve(__dirname, 'src/utils'),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+      "@utils/*": path.resolve(__dirname, "./src/utils/*"),
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react"],
+          "react-dom": ["react-dom"],
+          "react-router-dom": ["react-router-dom"],
+          "firebase/firestore": ["firebase/firestore"],
+        }
+      }
     }
   }
-
-})
+});
