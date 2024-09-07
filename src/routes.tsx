49f957d152a21288e5cc644c09404a819a0a7107
Leon Watson <leon@vlw2.com>
2024-09-07 04:12:20 -0500

    code splitting recipe page

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,13 +1,14 @@
 import { createBrowserRouter, LoaderFunction } from "react-router-dom";
 import { getRecipeBySlug, searchForRecipe } from "./firebase/actions";
-import { RecipePage } from "./components/RecipePage";
 import ErrorPage from "./components/ErrorPage";
 import { Root } from "./components/Root";
 import { PrivacyPolicy } from "./components/PrivacyPolicy";
-import { SEARCH_TERM_KEY } from "@Utils";
 import { RecipeListPage } from "./components/RecipeListPage/RecipeListPage";
 import { SearchPage } from "./components/SearchPage";
+import { SEARCH_TERM_KEY } from "@utils";
+import { lazy } from "react";
 
+const RecipePage = lazy(() => import("./components/RecipePage/RecipePage").then((module) => ({ default: module.RecipePage })));
 export const recipeLoader: LoaderFunction<{ slug: string }> = async ({
   params,
 }) => {
@@ -42,11 +43,11 @@ export const router = createBrowserRouter([
         path: "search",
         element: <SearchPage />,
         loader: async ({ request }) => {
-          const searchParams = new URL(request.url)
-          const searchTerm = searchParams.searchParams.get(SEARCH_TERM_KEY)
-          const recipes = await searchForRecipe(searchTerm || '')
-          return recipes
-        }
+          const searchParams = new URL(request.url);
+          const searchTerm = searchParams.searchParams.get(SEARCH_TERM_KEY);
+          const recipes = await searchForRecipe(searchTerm || "");
+          return recipes;
+        },
       },
       {
         path: "new",
